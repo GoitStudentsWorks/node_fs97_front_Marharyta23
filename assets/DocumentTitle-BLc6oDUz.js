b4b5ceabcd1ff16cc006b9e9108b02ad797b7906
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-15 07:54:52 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs97_front_Marharyta23@badd9ca0e3b3788ae2b77c363ba82ce49e490f05 🚀

---
+++ assets/DocumentTitle-BLc6oDUz.js
@@ -0,0 +1,2 @@
+import{j as o,H as t}from"./index-lXofKyVy.js";const e="_Logo_2egf4_1",n={Logo:e},r=()=>o.jsx(o.Fragment,{children:o.jsx("h2",{className:n.Logo,children:"AquaTrack"})}),a=r;function g({children:s}){return o.jsx(t,{children:o.jsx("title",{children:s})})}export{g as D,a as L};
+//# sourceMappingURL=DocumentTitle-BLc6oDUz.js.map
--- assets/DocumentTitle-kzJtAskX.js
+++
@@ -1,2 +0,0 @@
-import{j as o,H as t}from"./index-DruXbKkZ.js";const e="_Logo_2egf4_1",n={Logo:e},r=()=>o.jsx(o.Fragment,{children:o.jsx("h2",{className:n.Logo,children:"AquaTrack"})}),a=r;function g({children:s}){return o.jsx(t,{children:o.jsx("title",{children:s})})}export{g as D,a as L};
-//# sourceMappingURL=DocumentTitle-kzJtAskX.js.map
