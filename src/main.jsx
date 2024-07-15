badd9ca0e3b3788ae2b77c363ba82ce49e490f05
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-15 10:54:17 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <HelmetProvider>
         <Provider store={store}>
             <PersistGate persistor={persistor} loading={null}>
-                <BrowserRouter>
+                <BrowserRouter basename='/node_fs97_front_Marharyta23'>
                     <App />
                 </BrowserRouter>
             </PersistGate>
